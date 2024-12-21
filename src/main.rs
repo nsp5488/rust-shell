f20b6b51bc36f9550df77cb23f4761517336313d
nsp5488 <nsp5488@g.rit.edu>
2024-12-21 11:04:32 -0500

    Implemented type command, restructured commands to use command pattern

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+type Command = fn(&str) -> ();
+
+fn build_commands() -> HashMap<String, Command> {
+    let mut commands: HashMap<String, Command> = HashMap::new();
+
+    commands.insert("exit".to_string(), handle_exit);
+    commands.insert("echo".to_string(), handle_echo);
+    commands.insert("type".to_string(), handle_type);
+    return commands;
+}
+fn handle_exit(_command: &str) {
+    std::process::exit(0);
+}
 fn print_command_not_found(command: &str) {
     let result: String = format!("{}: command not found", command.trim());
     print!("{result}\n");
@@ -14,11 +28,23 @@ fn handle_echo(input: &str) {
     print!("{}", line.collect::<Vec<&str>>().join(" "));
 }
 
+fn handle_type(input: &str) {
+    let known_commands = build_commands();
+    let mut line = input.split(' ');
+
+    line.next(); // discard 'type'
+    let command = line.next();
+
+    match known_commands.get(command.unwrap().trim()) {
+        Some(_cmd) => print!("{} is a shell builtin\n", command.unwrap().trim()),
+        None => print_command_not_found(command.unwrap().trim()),
+    }
+}
 fn main() {
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
-
+    let commands: HashMap<String, fn(&str)> = build_commands();
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -28,10 +54,10 @@ fn main() {
 
         let mut line = input.split(' ');
 
-        match line.next().unwrap().trim() {
-            "exit" => return,
-            "echo" => handle_echo(&input),
-            _ => print_command_not_found(&input),
+        let func = commands.get(line.next().unwrap().trim());
+        match func {
+            Some(func) => func(&input),
+            None => print_command_not_found(&input),
         }
 
         io::stdout().flush().unwrap();
