83f4b48feb5085c5bc72e8c363c21cfdb3979524
nsp5488 <nsp5488@g.rit.edu>
2024-12-26 12:22:07 -0500

    Added more permissions to the write command

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,10 @@ use std::io::{self, Write};
 
 fn get_filewriter(file_path: &str, append: bool) -> Option<BufWriter<Box<dyn Write>>> {
     let file_result = OpenOptions::new()
-        .create(true)
         .append(append)
+        .read(true)
+        .write(true)
+        .create(true)
         .open(file_path);
     if let Ok(file) = file_result {
         return Some(BufWriter::new(Box::new(file)));
